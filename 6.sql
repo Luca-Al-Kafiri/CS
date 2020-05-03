4ac4c86bcd35bc519d77a20f63017abd265c832a
Luca <lucaalkafiri@gmail.com>
2020-05-03 22:45:08 +0400

    CS50 movies

---
+++ 6.sql
@@ -0,0 +1,1 @@
+SELECT AVG(rating) FROM ratings INNER JOIN movies ON movie_id = id WHERE year = 2012;
---
+++ 8.sql
@@ -0,0 +1,1 @@
+SELECT name FROM people INNER JOIN stars ON people.id = person_id INNER JOIN movies ON movie_id =movies.id WHERE title = "Toy Story";
